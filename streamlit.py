66047415a037fccb9b2e9de0383885bc757bd8d0
Dev-rajib <rajibsahadeveloper@gmail.com>
2026-07-08 16:17:59 +0530

    streamlit changes

---
+++ streamlit.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+import requests
+
+BASE_URL = "http://localhost:8000"
+
+st.set_page_config(
+
+)
+
+st.title()
+with st.sidebar:
+    pass
+
+st.header()
+
+st.markdown()
+question = st.text_input()
+if st.button("ask"):
+    pass
